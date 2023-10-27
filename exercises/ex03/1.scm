0f69eeee23239b05a33c93a61526d2b239a62895
Petko Petkov <petko.petkov987@gmail.com>
2023-10-27 09:16:35 +0300

    implemented repeat function

---
+++ exercises/ex03/1.scm
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (id x) x)
+
+(define (repeat n f)
+  (if (= n 0) id
+      (compose f (repeat (- n 1) f))))
+      ;(lambda (x) (f ((repeat (- n 1) f) x)))))
