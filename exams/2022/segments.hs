ac5edcf25bf38c65b9edbf22d1ec12b280c7bd3c
Petko Petkov <petko.petkov987@gmail.com>
2024-01-17 22:42:13 +0200

    added fillSegments function

--- exams/2022/segments.hs
+++ exams/2022/segments.hs
@@ -4,3 +4,6 @@ findSegment lst = foldl (\acc x -> if x < last acc then acc ++ [x] else acc) [he
 segments :: Ord a => [a] -> [[a]]
 segments [] = []
 segments lst = res : segments (drop (length res - 1) (tail lst)) where res = findSegment lst
+
+fillSegments :: (Num b, Enum b, Ord b) => [b] -> [b]
+fillSegments lst = concatMap (\x -> reverse [0..head x]) (segments lst)
